52799550c151c3d6da92f4031be45652e36be971
Max-Rus1977 <maxsvet77@yandex.ru>
2021-08-01 17:51:37 +0300

    second commit

--- comond.js
+++ comond.js
@@ -1,6 +1,9 @@
 const startBtn = document.querySelector('#id-start');
 const screens = document.querySelectorAll('.screen');
 const timeBtns = document.querySelector('#id-time-list');
+const timeEl = document.querySelector('#id-time');
+
+let time = 11;
 
 startBtn.addEventListener('click', (event) => {
   event.preventDefault();
@@ -9,6 +12,26 @@ startBtn.addEventListener('click', (event) => {
 
 timeBtns.addEventListener('click', (event) => {
   if (event.target.classList.contains('time-btn')) {
-    console.log(event.target.getAttribute('data-time'));
+    let time = +(event.target.getAttribute('data-time'));
+    startGame();
   }
 })
+
+startGame();
+
+function startGame() {
+  setInterval(decriaseTime, 1000)
+  screens[1].classList.add('up');
+  timeEl.innerHTML = `00:${time}`;
+}
+
+function decriaseTime() {
+  let curentTime = --time;
+  if (curentTime < 10) {
+    curentTime = `0${curentTime}`;
+  }
+  timeEl.innerHTML = `00:${curentTime}`;
+}
+
+// let curentTime = --time;
+//   
