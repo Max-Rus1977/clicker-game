c5b6c87f63f244e3d897fdfdeef8da0c08ee5ff4
Max-Rus1977 <maxsvet77@yandex.ru>
2021-08-02 12:24:02 +0300

    add random calors and btn 40s

--- comond.js
+++ comond.js
@@ -4,6 +4,8 @@ const timeBtns = document.querySelector('#id-time-list');
 const timeEl = document.querySelector('#id-time');
 const board = document.querySelector('#id-board');
 
+const colors = ['#d33e9e', '#8e44ad', '#74b8e5', '#d9ac83', '#2ecc71', '#cd1c1c'];
+
 let time = 0;
 let score = 0;
 
@@ -74,14 +76,25 @@ function creatRandomCircles() {
   const y = getRandomSize(0, boardHeight - size);
   /* -----------  */
 
+  const color = getRandomColor();
+
   circle.classList.add('circle');
+
+  circle.style.backgroundColor = color;
   circle.style.width = `${size}px`;
   circle.style.height = `${size}px`;
   circle.style.top = `${y}px`;
   circle.style.left = `${x}px`;
+
   board.append(circle);
+
 }
 
 function getRandomSize(min, max) {
   return Math.round(Math.random() * (max - min) + min)
+}
+
+function getRandomColor() {
+  const index = Math.floor(Math.random() * colors.length);
+  return colors[index];
 }
